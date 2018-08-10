e042af5676466f887abc0a0bc2858335a5ff29fa
Parth Patel <patelparth14697@gmail.com>
2018-08-10 18:23:08 +1000

    PayFineUI.java V1
    Changed memStr to memberString to make it more meaningful.

--- src/PayFineUI.java
+++ src/PayFineUI.java
@@ -32,13 +32,13 @@ public class PayFineUI {
 			switch (state) {
 			
 			case READY:
-				String memStr = input("Swipe member card (press <enter> to cancel): ");
-				if (memStr.length() == 0) {
+				String memberString = input ("Swipe member card (press <enter> to cancel): "); //memStr to memberString and increased space.
+				if (memberString.length() == 0) { //memStr to memberString.
 					control.cancel();
 					break;
 				}
 				try {
-					int memberId = Integer.valueOf(memStr).intValue();
+					int memberId = Integer.valueOf(memberString).intValue(); //memStr to memberString.
 					control.cardSwiped(memberId);
 				}
 				catch (NumberFormatException e) {
